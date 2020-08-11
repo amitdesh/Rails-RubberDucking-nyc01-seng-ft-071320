0999db11b739dc49bb7746108a5c29c7e4730d9e
amitdesh <amitdesh2014@gmail.com>
2020-08-11 11:36:06 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,53 @@
 class DucksController < ApplicationController
+  before_action :find_duck, only: [:show, :edit, :update]
 
   def index
     @ducks = Duck.all
+  end
+
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck = Duck.new
+    @students = Student.all
+  end
+
+  def create
+    @duck = Duck.create(duck_params)
+
+    if @duck.valid?
+      redirect_to duck_path(@duck)
+    else
+      flash[:my_errors] = @duck.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @duck.update(duck_params)
+
+    if @duck.valid?
+      redirect_to duck_path(@duck)
+    else
+      flash[:my_errors] = @duck.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student_id)
+  end
+
+  def find_duck
+    @duck = Duck.find(params[:id])
   end
 
 end
---
+++ db/migrate/20200811150600_add_integer_to_ducks.rb
@@ -0,0 +1,5 @@
+class AddIntegerToDucks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :ducks, :student_id, :integer
+  end
+end
